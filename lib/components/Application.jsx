59f7acd30b0f8eca3cb5d331d1ca648373f3b5e8
Elijah Williams <ejwill04@gmail.com>
2017-01-11 10:16:08 -0700

    copied test and function over

--- lib/components/Application.jsx
+++ lib/components/Application.jsx
@@ -1,11 +1,13 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import scoreWord from '../helperFunctions/scoreWord';
 
 export default class Org extends Component {
   render() {
     return (
       <div>
-        Oh Hey
+        <h1>Scrabble</h1>
+        <div>Your score: { scoreWord('word') }</div>
       </div>
-    )
+    );
   }
 }
--- test/Application.spec.js
+++ test/Application.spec.js
@@ -1,10 +1,28 @@
-import React from 'react';
+import scoreWord from '../lib/helperFunctions/scoreWord';
+import Application from '../lib/components/Application';
 
 import { shallow, mount, render } from 'enzyme';
 import { expect, assert } from 'chai';
+import React from 'react';
 
-describe('', () => {
-  it('', () => {
-    console.log('Ahem...');  // eslint-disable-line no-console
+describe('scoreWord', () => {
+  it('should return a score when a string is entered', () => {
+    expect(scoreWord('word')).to.equal(8);
+  });
+
+  it('should multiple the score if multiplier is passed', () => {
+    expect(scoreWord('word', 3)).to.equal(24);
+  });
+
+  it('should return 0 if null or empty string', () => {
+    expect(scoreWord(null)).to.equal(0);
+    expect(scoreWord('')).to.equal(0);
+  });
+});
+
+describe('Application', () => {
+  it('should render a div', () => {
+    const wrapper = shallow(<Application />);
+    assert.equal(wrapper.type(), 'div');
   });
 });
