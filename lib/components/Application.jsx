cac89c472f910f39cf7b506596e1877555372687
Elijah Williams <ejwill04@gmail.com>
2017-01-11 13:01:28 -0700

    After eval

--- lib/components/Application.jsx
+++ lib/components/Application.jsx
@@ -1,12 +1,32 @@
 import React, { Component } from 'react';
 import scoreWord from '../helperFunctions/scoreWord';
+import Score from './Score';
 
 export default class Org extends Component {
+  constructor() {
+    super();
+    this.state = {
+      score: 0,
+    };
+  }
+
+  recordScore(e) {
+    const score = scoreWord(e.target.value);
+    this.setState({ score });
+  }
+
   render() {
     return (
       <div>
-        <h1>Scrabble</h1>
-        <div>Your score: { scoreWord('word') }</div>
+        <input
+          placeholder='Word'
+          onChange={
+            this.recordScore.bind(this)
+          }
+          />
+        <Score
+          usersScore={this.state.score}
+          />
       </div>
     );
   }
